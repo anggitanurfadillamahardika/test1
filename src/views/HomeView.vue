fd20286ee87291d17ed2d5e385aa4bbb99bac557
Anggita Mahardika <anggita.mahardika97@gmail.com>
2022-12-08 16:36:30 +0700

    HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,77 +1,79 @@
 <template>
   <div>
     <form @submit.prevent="add">
-        <input type="hidden" v-model="form.id">
-        <input type="text" v-model="form.name">
-        <button type="submit" v-show="!updateSubmit">add</button>  
-        <button type="button" v-show="updateSubmit" @click="update(form)">Update</button> 
+        <input type="hidden" v-model="state.form.id">
+        <input type="text" v-model="state.form.name">
+        <button type="submit" v-show="!state.updateSubmit">add</button>  
+        <button type="button" v-show="state.updateSubmit" @click="onUpdate(form)">Update</button> 
     </form>
-    <ul v-for="user in users" :key="user.idd">
+    <ul v-for="user in state.users" :key="user.id">
       <li>
       <span>{{user.name}}</span> &#160;
-      <button @click="edit(user)">Edit</button> ||  <button @click="del(user)">Delete</button>
+      <button @click="onEdit(user)">Edit</button> ||  <button @click="onDel(user)">Delete</button>
       </li>
     </ul>
   </div>
 </template>
 
-<script>
+<script setup lang="ts">
 /* eslint-disable */ 
+import {reactive} from 'vue-demi'
 import axios from 'axios'
-export default {
-  data(){
-    return{
-        form: {
-          id: '',
-          name: ''
-        },
-        users: '',
-        updateSubmit: false
+
+  const state = reactive ({
+    form: {
+      id: '',
+      name: ''
+    },
+    users: '',
+    updateSubmit: false
+    })
+
+ const onMounted = () => {
+  let result = axios
+    .get("https://reqres.in/api/users")
+    .then(result => {
+      state.form = result.data.data
+    })
+    .catch(error => {
+      console.log(error);
+    })
+}
+onMounted();
+
+
+const onAdd = () => {
+      axios.post('https://reqres.in/api/users/', state.form).then(res => {
+          onMounted()
+          state.form.name = ''
+      })
     }
-  },
-  mounted() {
-    this.load()
-  },
-  methods: {
-    load(){
-        axios.get('https://reqres.in/api/users/').then(res => {
-        this.users = res.data
-      }).catch ((err) => {
-        console.log(err);
-        
-      })
-    },
-      add(){
-      axios.post('https://reqres.in/api/users/', this.form).then(res => {
-          this.load()
-          this.form.name = ''
-      })
-    },
-    edit(user){ 
-        this.updateSubmit = true
-        this.form.id = user.id 
-        this.form.name = user.name 
-    },
-    update(form){ 
-       return axios.put('https://reqres.in/api/users/' + form.id , {name: this.form.name}).then(res => {
-        this.load()
-        this.form.id = ''
-        this.form.name = ''
-        this.updateSubmit = false
+
+const onEdit = (user) => {
+        state.updateSubmit = true
+        state.form.id = user.id 
+        state.form.name = user.name 
+    }
+
+const  onUpdate = (form) => { 
+       return axios.put('https://reqres.in/api/users/' + form.id , {name: state.form.name}).then(res => {
+        onMounted()
+        state.form.id = ''
+        state.form.name = ''
+        state.updateSubmit = false
       }).catch((err) => {
         console.log(err);
         
       })
-    },
-    del(user){
+    }
+
+const onDel = (user) => {
       axios.delete('https://reqres.in/api/users/' + user.id).then(res =>{
-          this.load()
-          let index = this.users.indexOf(form.name)
-          this.users.splice(index,1)
+          onMounted()
+          let index = state.users.indexOf(state.form.name)
+          state.users.splice(index,1)
       })
     }
-  }
-}
 </script>
 
 
